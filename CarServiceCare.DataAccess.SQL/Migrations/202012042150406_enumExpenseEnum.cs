9076a7c2f86f8b28e51e7461ceae758d45138e20
Harpyn <d.buch@centrum.cz>
2020-12-04 22:52:03 +0100

    fix for migration update

---
+++ CarServiceCare.DataAccess.SQL/Migrations/202012042150406_enumExpenseEnum.cs
@@ -0,0 +1,18 @@
+namespace CarServiceCare.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class enumExpenseEnum : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Expenses", "Type", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Expenses", "Type", c => c.String());
+        }
+    }
+}
